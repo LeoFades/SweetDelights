88aa0cc5eae295c88d141c5a3ffda1b81fd6e318
Leo Fades <137497080+LeoFades@users.noreply.github.com>
2025-12-06 20:46:18 +0800

    base project upload

---
+++ mainApp/resources/js/lib/utils.ts
@@ -0,0 +1,18 @@
+import { InertiaLinkProps } from '@inertiajs/react';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
+export function isSameUrl(
+    url1: NonNullable<InertiaLinkProps['href']>,
+    url2: NonNullable<InertiaLinkProps['href']>,
+) {
+    return resolveUrl(url1) === resolveUrl(url2);
+}
+
+export function resolveUrl(url: NonNullable<InertiaLinkProps['href']>): string {
+    return typeof url === 'string' ? url : url.url;
+}
